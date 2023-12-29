50da0bd23e38bd070069b7d0c4af5894e6225f82
Kamaz-Birr <yohan.orewa>
2023-12-29 16:55:23 +0100

    Initial Commit

---
+++ Stanford_Swift_UI_01/Stanford_Swift_UI_01App.swift
@@ -0,0 +1,17 @@
+//
+//  Stanford_Swift_UI_01App.swift
+//  Stanford_Swift_UI_01
+//
+//  Created by Haldox on 29/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Stanford_Swift_UI_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
