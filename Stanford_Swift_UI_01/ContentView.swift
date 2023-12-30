1a71c8c1bf8ce3ef0d45b3a69853eb42b6d42de6
Kamaz-Birr <yohan.orewa>
2023-12-31 00:14:19 +0100

    ver 1.0

--- Stanford_Swift_UI_01/ContentView.swift
+++ Stanford_Swift_UI_01/ContentView.swift
@@ -9,13 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        // The VStack function returns a TupleView ie Views in a Tuple data type
+        // The VStack function below specifically returns a tuple with 2 items; Image & text
+        // The @ViewBuilder turns the list of views into a TupleView
+//        VStack(content: {
+            // The functions .imageScale() & .foregroundStyle() are called View modifiers and they return a View
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+//        })
+//        .padding()
+        
+        HStack {
+            CardView()
+            CardView(isFaceUp: true)
         }
+        .foregroundStyle(.indigo)
         .padding()
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        if isFaceUp {
+            ZStack {
+                RoundedRectangle(cornerRadius: 25.0)
+                    .foregroundStyle(.white)
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                    .strokeBorder(lineWidth: 5.0)
+                Text("👻")
+                    .font(.largeTitle)
+            }
+            .foregroundStyle(.indigo)
+        } else {
+            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
